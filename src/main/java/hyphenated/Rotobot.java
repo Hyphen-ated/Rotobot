05e3e94d959c75ebd78b6f04889bc0c4b8884a35
Hyphen-ated <Hyphen-ated@users.noreply.github.com>
2022-09-01 20:32:30 -0700

    use dotenv

--- src/main/java/hyphenated/Rotobot.java
+++ src/main/java/hyphenated/Rotobot.java
@@ -1,25 +1,23 @@
 package hyphenated;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
-
 public class Rotobot {
 
-    private static final String TOKEN_PATH = "./token.txt";
 
     public static void main(String[] args) throws Exception {
-        String token = Files.readString(Path.of(TOKEN_PATH));
+        Dotenv dotenv = Dotenv.load();
+        String token = dotenv.get("DISCORD_BOT_TOKEN");
         JDA api = JDABuilder.createDefault(token)
                 .addEventListeners(new PickCommand())
                 .build();
         api.updateCommands().addCommands(
                 Commands.slash("pick", "Picks a card in the current draft")
-                        .addOption(OptionType.STRING, "card", "The card to pick")
+                        .addOption(OptionType.STRING, "card", "The card to pick", true)
         ).queue();
     }
 }
