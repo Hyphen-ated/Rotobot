1efbfe74d256c6e35056864073e7ca5e6480b8d2
Hyphen-ated <Hyphen-ated@users.noreply.github.com>
2022-09-01 20:26:43 -0700

    put in a stub slash command

---
+++ src/main/java/hyphenated/PickCommand.java
@@ -0,0 +1,28 @@
+package hyphenated;
+
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.commands.OptionMapping;
+import org.apache.commons.lang3.StringUtils;
+
+public class PickCommand extends ListenerAdapter {
+    @Override
+    public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
+        if (event.getName().equals("pick")) {
+            OptionMapping mapping = event.getOption("card");
+            String card;
+            if (mapping == null) {
+                card = null;
+            } else {
+                card = mapping.getAsString();
+            }
+            String reply;
+            if (StringUtils.isBlank(card)) {
+                reply = "You need to pick a card";
+            } else {
+                reply = "Ah, " + card + ", nice choice.";
+            }
+            event.reply(reply).queue();
+        }
+    }
+}
--- src/main/java/hyphenated/Rotobot.java
+++ src/main/java/hyphenated/Rotobot.java
@@ -1,8 +1,25 @@
 package hyphenated;
 
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.Commands;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class Rotobot {
 
-    public static void main(String[] args) {
-        System.out.println("We in there");
+    private static final String TOKEN_PATH = "./token.txt";
+
+    public static void main(String[] args) throws Exception {
+        String token = Files.readString(Path.of(TOKEN_PATH));
+        JDA api = JDABuilder.createDefault(token)
+                .addEventListeners(new PickCommand())
+                .build();
+        api.updateCommands().addCommands(
+                Commands.slash("pick", "Picks a card in the current draft")
+                        .addOption(OptionType.STRING, "card", "The card to pick")
+        ).queue();
     }
 }
