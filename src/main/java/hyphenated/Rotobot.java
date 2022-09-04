efb035bfd3beaa47aa07492efa1019d050efa97c
Hyphen-ated <Hyphen-ated@users.noreply.github.com>
2022-09-04 01:45:48 -0700

    add autocomplete for vintage cards

--- src/main/java/hyphenated/Rotobot.java
+++ src/main/java/hyphenated/Rotobot.java
@@ -1,5 +1,9 @@
 package hyphenated;
 
+import com.google.common.collect.Sets;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.stream.JsonReader;
 import hyphenated.commands.PickCommand;
 import hyphenated.commands.UpdateScryfallCommand;
 import net.dv8tion.jda.api.JDA;
@@ -7,26 +11,87 @@ import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.interactions.commands.DefaultMemberPermissions;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
+import net.gcardone.junidecode.Junidecode;
+import org.apache.commons.collections4.Trie;
+import org.apache.commons.collections4.trie.PatriciaTrie;
 
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
+import java.util.Locale;
+import java.util.Set;
+
 
 public class Rotobot {
+    public static final Trie<String, String> vintageCardTrie = new PatriciaTrie<>();
 
     public static void main(String[] args) throws Exception {
+        getLegalCardsFromScryfallData("vintage");
+
         JDA api = JDABuilder.createDefault(Config.DISCORD_BOT_TOKEN)
                 .addEventListeners(new PickCommand())
                 .addEventListeners(new UpdateScryfallCommand())
                 .build();
         api.updateCommands().addCommands(
                 Commands.slash(PickCommand.CMD, "Picks a card in the current draft")
-                        .addOption(OptionType.STRING, "card", "The card to pick", true),
+                        .addOption(OptionType.STRING, "card", "The card to pick", true, true),
+
                 Commands.slash(UpdateScryfallCommand.CMD, "Download the latest scryfall data")
                         .setDefaultPermissions(DefaultMemberPermissions.DISABLED)
         ).queue();
+
     }
 
-    public static List<String> getLegalCardsFromScryfallData(String format) {
-        //JSONObject fullJson = new JSONObject(IOUtils.file(scryfallEndpoint, StandardCharsets.UTF_8));
+
+    // for these kinds of cards we want to ignore the part after the // in the name.
+    // for other kinds, e.g. "Fire // Ice", we want to keep the whole thing as is
+    static final Set<String> layoutsToIgnoreBack = Sets.newHashSet(
+            "transform",
+            "flip",
+            "adventure",
+            "modal_dfc");
+
+    public static List<String> getLegalCardsFromScryfallData(String format) throws Exception {
+        Gson gson = new Gson();
+        JsonReader reader = new JsonReader(
+                Files.newBufferedReader(Path.of(Config.SCRYFALL_DEFAULT_CARDS_PATH),
+                                        StandardCharsets.UTF_8));
+
+        reader.beginArray();
+
+        while (reader.hasNext()) {
+            JsonObject cardJson = gson.fromJson(reader, JsonObject.class);
+            String cardName = cardJson.get("name").getAsString();
+            cardName = Junidecode.unidecode(cardName);
+            String layout = cardJson.get("layout").getAsString();
+            if (layoutsToIgnoreBack.contains(layout)) {
+                cardName = extractFirstCardName(cardName);
+            }
+            String legality = cardJson.get("legalities").getAsJsonObject()
+                                      .get(format).getAsString();
+            if (!"legal".equals(legality) && !"restricted".equals(legality)) {
+                continue;
+            }
+
+            String lowerName = cardName.toLowerCase(Locale.ROOT);
+            if(vintageCardTrie.containsKey(lowerName)) {
+                continue;
+            }
+            vintageCardTrie.put(lowerName, cardName);
+        }
+        reader.endArray();
+        reader.close();
+
         return null;
     }
+
+    private static String extractFirstCardName(String cardName) {
+        int idx = cardName.indexOf(" // ");
+        if (idx > -1) {
+            return cardName.substring(0, idx);
+        }
+        throw new RuntimeException("Expected to find // in card: " + cardName);
+    }
+
 }
--- src/main/java/hyphenated/commands/PickCommand.java
+++ src/main/java/hyphenated/commands/PickCommand.java
@@ -1,9 +1,17 @@
 package hyphenated.commands;
 
+import hyphenated.Rotobot;
+import net.dv8tion.jda.api.events.interaction.command.CommandAutoCompleteInteractionEvent;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.commands.Command;
 import net.dv8tion.jda.api.interactions.commands.OptionMapping;
 import org.apache.commons.lang3.StringUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.SortedMap;
+import java.util.stream.Stream;
 
 public class PickCommand extends ListenerAdapter {
     public static final String CMD = "pick";
@@ -24,6 +32,25 @@ public class PickCommand extends ListenerAdapter {
                 reply = "Ah, " + card + ", nice choice.";
             }
             event.reply(reply).queue();
+        }
+    }
+
+    @Override
+    public void onCommandAutoCompleteInteraction(CommandAutoCompleteInteractionEvent event) {
+        if (event.getName().equals(CMD) && event.getFocusedOption().getName().equals("card")) {
+            String value = event.getFocusedOption().getValue();
+            SortedMap<String, String> cards = Rotobot.vintageCardTrie.prefixMap(value);
+            List<Command.Choice> options = new ArrayList<>(25);
+            int i = 0;
+            for(String name : cards.values()) {
+                options.add(new Command.Choice(name, name));
+                ++i;
+                if (i >= 25) {
+                    break;
+                }
+            }
+
+            event.replyChoices(options).queue();
         }
     }
 }
